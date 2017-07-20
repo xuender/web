0baacbfb1731ee70dcd1e4ff91bb348e96bb01a3
徐晨阳 <xuender@gmail.com>
2017-07-20 14:45:25 +0800

    fonts

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
 const extractSass = new ExtractTextPlugin({
-	filename: "[name].[hash:8].css",
+	filename: "static/[name].[hash:8].css",
 	disable: process.env.NODE_ENV === "development"
 });
 declare var __dirname;
@@ -11,12 +11,11 @@ declare var __dirname;
 const config: webpack.Configuration = {
 	entry: {
 		app: './src/index.js',
-		// app: './src/index.ts',
 		vendors: ['jquery'],
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name]-[hash:8].js'
+		filename: 'static/[name]-[hash:8].js'
 	},
 	module: {
 		loaders: [
@@ -37,7 +36,7 @@ const config: webpack.Configuration = {
 					fallback: "style-loader"
 				})
 			},
-			{
+			{	// SASS
 				test: /\.scss$/,
 				use: extractSass.extract({
 					use: [{
@@ -48,31 +47,14 @@ const config: webpack.Configuration = {
 					fallback: "style-loader"
 				})
 			},
-			{
+			{	// Typescript
 				test: /\.ts/,
 				loader: 'ts-loader'
 			},
-			// Fonts
-			{
-				test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-				loaders: ['url-loader?name=static/[name]-[hash:8].[ext]']
-			},
-			{
-				test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-				loaders: ['url-loader?name=static/[name]-[hash:8].[ext]']
-			},
-			{
-				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-				loaders: ['url-loader?name=static/[name]-[hash:8].[ext]']
-			},
-			{
-				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+			{	// Fonts
+				test: /\.(eot|svg|ttf|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
 				loaders: ['file-loader?name=static/[name]-[hash:8].[ext]']
 			},
-			{
-				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-				loaders: ['url-loader?name=static/[name]-[hash:8].[ext]']
-			}
 		]
 	},
 	plugins: [
